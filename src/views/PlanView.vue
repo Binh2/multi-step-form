00a9937a11f408cc9ec7e53923505e9a5e97b3fd
Binh2 <hgqbinh2002@gmail.com>
2023-01-19 08:06:55 +0700

    Unfinished

--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Plan</h1>
+  <form>
+    <h1>Select your plan</h1>
+    <!-- Arcade
+  $9/mo
+  
+  Advanced
+  $12/mo
+  
+  Pro
+  $15/mo
+  
+  Monthly
+  Yearly
+  
+  Go Back -->
+    <button class="form__next-page-button" type="button">Next Step</button>
+  </form>
 </template>
